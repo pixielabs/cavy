4a0cbb73539b0b81a8b328533b20c4eceb1da9a1
Tyler Pate <tyler@21.co>
2017-03-20 20:46:34 -0700

    sample-app: show new suite imports, test disable, new props

--- sample-app/EmployeeDirectory/index.ios.js
+++ sample-app/EmployeeDirectory/index.ios.js
@@ -5,17 +5,31 @@ import { Tester, TestHookStore } from 'cavy';
 
 import EmployeeDirectoryApp from './app/EmployeeDirectoryApp';
 
-import EmployeeListSpec from './specs/EmployeeListSpec';
+import GLOBAL from './app/helpers/globals.js';
 
-const testHookStore = new TestHookStore();
+if (GLOBAL.TEST_ENABLED) {
+  var testHookStore = new TestHookStore();
+  var testSuites = require('./specs/itSuites.js');
+  var testSuitesArray = [testSuites.filterEmployeeList, testSuites.tapAndEmail];
+}
 
 class AppWrapper extends Component {
   render() {
-    return (
-      <Tester specs={[EmployeeListSpec]} store={testHookStore} waitTime={1000}>
-        <EmployeeDirectoryApp />
-      </Tester>
-    );
+    if (GLOBAL.TEST_ENABLED) {
+      return (
+        <Tester 
+          suites={testSuitesArray} 
+          store={testHookStore} 
+          waitTime={1000}
+          testStartDelay={1000}
+          consoleLog={true} // {false}, {true}, 'verbose'
+        >
+          <EmployeeDirectoryApp />
+        </Tester>
+      );
+    } else {
+      return (<EmployeeDirectoryApp />);
+    }
   }
 }
 
