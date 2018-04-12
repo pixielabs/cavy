5f7eb7f0dceb62b1b16712a20cf44e37d648e8f7
David Somers <david@jalada.co.uk>
2018-04-12 12:55:02 +0100

    Add unit tests (#88)
    Closes #66 and #67. Thanks @mcdaniel67!

---
+++ src/__test__/TestHookStore.test.js
@@ -0,0 +1,41 @@
+import TestHookStore from '../TestHookStore'
+
+describe("TestHookStore", () => {
+  it("initializes hooks by default", () => {
+    const testHookStore = new TestHookStore();
+
+    expect(testHookStore.hooks).toBeDefined();
+  });
+
+  it("can add components", () => {
+    const testHookStore = new TestHookStore();
+    testHookStore.add("key", "value");
+
+    expect(testHookStore.get("key")).toEqual("value");
+  })
+
+  it("warns when adding an existing component", () => {
+    const testHookStore = new TestHookStore();
+    testHookStore.add("key", "value1");
+    const warnSpy = jest.spyOn(global.console, 'warn');
+    testHookStore.add("key", "value2");
+
+    expect(warnSpy).toBeCalled();
+  });
+
+  it("overrides when adding an existing component", () => {
+    const testHookStore = new TestHookStore();
+    testHookStore.add("key", "value1");
+    testHookStore.add("key", "value2");
+
+    expect(testHookStore.get("key")).toEqual("value2");
+  });
+
+  it("can delete components", () => {
+    const testHookStore = new TestHookStore();
+    testHookStore.add("key", "value");
+    testHookStore.remove("key");
+
+    expect(testHookStore.get("key")).toBeUndefined();
+  });
+});
