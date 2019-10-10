bc677b0afb51fa22da46bb3ec566b82f2fea6453
Abigail McPhillips <abigail.mcphillips@gmail.com>
2019-10-10 11:10:31 +0100

    Test function components have testable props with wrap

--- sample-app/CavyDirectory/specs/EmployeeListSpec.js
+++ sample-app/CavyDirectory/specs/EmployeeListSpec.js
@@ -1,3 +1,5 @@
+import { hasButtonText } from './helpers';
+
 export default function(spec) {
   spec.describe('Listing the employees', function() {
     spec.it('filters the list by search input', async function() {
@@ -14,6 +16,8 @@ export default function(spec) {
       await spec.press('EmployeeList.AmyTaylor');
       await spec.pause(1000);
       await spec.exists('ActionBar.EmailButton');
+      const button = await spec.findComponent('ActionBar.EmailButton');
+      await hasButtonText(button, 'email');
     });
   });
 
