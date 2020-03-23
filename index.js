bbfabda83200b16d821765970d53367b66577972
Luca Micieli <rams23@live.it>
2020-03-23 13:04:52 +0100

    Add cavyCreateElement jsx wrapper

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import Tester from './src/Tester';
 import TestHookStore from './src/TestHookStore';
 import useCavy from './src/useCavy';
 import wrap from './src/wrap';
+import cavyCreateElement, { setJSXConfig, Fragment } from './src/cavyCreateElement';
 
 const Cavy = {
   hook,
   Tester,
   TestHookStore,
   useCavy,
-  wrap
+  wrap,
+  cavyCreateElement,
+  setJSXConfig,
+  Fragment
 };
 
 module.exports = Cavy;
