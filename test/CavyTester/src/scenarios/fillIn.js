a956b24559c62c71da4544d556ed0e179f95c712
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-05-15 12:06:04 +0100

    Update fillIn scenario

--- test/CavyTester/src/scenarios/fillIn.js
+++ test/CavyTester/src/scenarios/fillIn.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { TextInput } from 'react-native';
+import { Text, TextInput } from 'react-native';
 import { useCavy } from 'cavy';
 
 export const key = 'FillIn';
 
-const testId = `${key}.TextInput`;
+const textInputId = `${key}.TextInput`;
+const textId = `${key}.Text`;
+const textText = 'text';
 
 export const Screen = () => {
   const [text, setText] = useState('');
@@ -13,10 +15,15 @@ export const Screen = () => {
   return (
     <>
       <TextInput
-        ref={generateTestHook(testId)}
+        ref={generateTestHook(textInputId)}
         value={text}
         onChangeText={value => setText(value)}
       />
+      {text === textText && (
+        <Text ref={generateTestHook(textId)}>
+          I only show up if fillIn succeeded
+        </Text>
+      )}
     </>
   );
 };
@@ -25,6 +32,8 @@ export const label = 'spec.fillIn fills in a component that responds to onChange
 export const spec = spec =>
   spec.describe(key, () =>
     spec.it(label, async () => {
-      await spec.fillIn(testId, 'text');
+      await spec.notExists(textId);
+      await spec.fillIn(textInputId, textText);
+      await spec.exists(textId);
     })
   );
