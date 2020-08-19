691f102189336f014313644014404d0f7c84db74
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-08-19 15:16:25 +0100

    Add test for useRef

--- test/CavyTester/src/App.js
+++ test/CavyTester/src/App.js
@@ -14,6 +14,7 @@ import * as findComponent from './scenarios/findComponent';
 import * as focus from './scenarios/focus';
 import * as notExists from './scenarios/notExists';
 import * as taggingTest from './scenarios/taggingTest';
+import * as useRef from './scenarios/useRef';
 // Import new scenarios here:
 
 // Add new scenarios here:
@@ -28,7 +29,8 @@ const scenarios = [
   findComponent,
   focus,
   notExists,
-  taggingTest
+  taggingTest,
+  useRef
 ];
 
 // Validate scenarios.
