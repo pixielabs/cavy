65214431ad4146454b993d2dff66d1f4329916ca
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-10-01 12:18:19 +0100

    WIP upgrade react-native 0.63

--- sample-app/CavyDirectory/index.js
+++ sample-app/CavyDirectory/index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-
 import EmployeeDirectoryApp from './app/EmployeeDirectoryApp';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => EmployeeDirectoryApp);
