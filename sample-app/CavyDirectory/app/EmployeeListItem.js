55d7b90e55189c8518da6871a3947de6ca13fb3b
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-10-01 17:11:39 +0100

    Fix cavy tests

--- sample-app/CavyDirectory/app/EmployeeListItem.js
+++ sample-app/CavyDirectory/app/EmployeeListItem.js
@@ -5,13 +5,14 @@ import { useCavy, wrap } from 'cavy';
 export default ({ data, navigation }) => {
   const generateTestHook = useCavy();
   const WrappedText = wrap(Text);
+  const WrappedTouchableHighlight = wrap(Text);
 
   const navigateTo = (id) => {
     navigation.navigate('EmployeeDetails', {employeeId: id})
   }
 
   return (
-    <TouchableHighlight
+    <WrappedTouchableHighlight
       ref={generateTestHook(`EmployeeList.${data.firstName}${data.lastName}`)}
       onPress={() => navigateTo(data.id)}
       underlayColor={'#EEEEEE'}>
@@ -27,7 +28,7 @@ export default ({ data, navigation }) => {
           </WrappedText>
         </View>
       </View>
-    </TouchableHighlight>
+    </WrappedTouchableHighlight>
   )
 };
 
