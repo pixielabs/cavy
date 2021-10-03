123ce282bbd4f3cd214166eac2575fef35f001d0
Jalada <126989+jalada@users.noreply.github.com>
2021-10-03 17:21:43 +0100

    Move sample app

--- sample-app/CavyDirectory/specs/helpers.js
+++
@@ -1,6 +0,0 @@
-export async function hasButtonText(button, expected) {
-  const actual = button.props.text;
-  if (actual != expected) {
-    throw new Error(`Button text is ${actual}, not ${expected}`);
-  }
-}
